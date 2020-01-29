ee1e5bfec94733dae7f3e8bbfc1c4f970165973a
Christian Diederich <cdiede@t-online.de>
2020-01-30 00:15:27 +0100

    constructor pattern

--- example.js
+++ example.js
@@ -1,24 +1,5 @@
-const calculator = require('./index.js');
+const League = require('./index.js');
 
-const matches = [
-  {
-    teams: ['BVB', 'Bayern'],
-    attempts: [9, 18]
-  },
-  {
-    teams: ['BVB', 'Freiburg'],
-    attempts: [16, 4]
-  },
-  {
-    teams: ['Bayern', 'Freiburg'],
-    attempts: [16, 2]
-  }
-];
+const league = new League(['BVB', 'Bayern', 'Freiburg']);
 
-const teams = calculator.getTeams(matches);
-
-console.log(teams);
-
-const skillTable = calculator.getSkillTable(teams, matches);
-
-console.log(skillTable);
+console.log(league.teams);
--- index.js
+++ index.js
@@ -1,75 +1,109 @@
-/**
- * Calculates linear regression
- * @param {Array} xArr
- * @param {Array} yArr
- * @return {object} {a, b}
- */
-const getLinearRegression = function(xArr, yArr) {
-  let xAvg = 0;
-  let yAvg = 0;
-  const xx = xArr;
-  const yy = yArr;
-  const xxyy = [];
-  const xx2 = [];
-  let xxyySum = 0;
-  let xx2Sum = 0;
-  if (xArr.length < 1 || xArr.length !== yArr.length) return null;
-  for (let i = 0; i < xArr.length; i++) {
-    xAvg = xAvg + xArr[i];
-    yAvg = yAvg + yArr[i];
-  }
-  xAvg = xAvg / xArr.length;
-  yAvg = yAvg / yArr.length;
-  for (let i = 0; i < xArr.length; i++) {
-    xx[i] = xArr[i] - xAvg;
-    yy[i] = yArr[i] - yAvg;
-    xxyy.push(xx[i] * yy[i]);
-    xx2.push(xx[i] * xx[i]);
-    xxyySum = xxyySum + xxyy[i];
-    xx2Sum = xx2Sum + xx2[i];
-  }
-  const b = xxyySum / xx2Sum;
-  const a = yAvg - b * xAvg;
-  return { a, b };
-};
+module.exports = (() => {
+  /**
+   * Constructor function
+   * @param {Array} teams
+   */
+  function Constructor(teams) {
+    this.teams = teams.map(e => ({
+      id: e,
+      form: null,
+      attemptsPerGoal: null,
+      basicSkill: null,
+      homeAdvantage: null,
+      linRegAttempts: {
+        a: null,
+        b: null
+      },
+      linRegCounterAttempts: {
+        a: null,
+        b: null
+      }
+    }));
+    this.matches = [];
+  };
 
-/**
- * Returns an array of all team names
- * @param {Array} matches
- * @return {Array}
- */
-const getTeams = matches => {
-  const teams = [];
-  matches.forEach(match => {
-    if (!teams.includes(match.teams[0])) teams.push(match.teams[0]);
-    if (!teams.includes(match.teams[1])) teams.push(match.teams[1]);
-  });
-  return teams;
-};
+  /**
+   * Calculates linear regression
+   * @param {Array} xArr
+   * @param {Array} yArr
+   * @return {object} {a, b}
+   */
+  const getLinearRegression = function(xArr, yArr) {
+    let xAvg = 0;
+    let yAvg = 0;
+    const xx = xArr;
+    const yy = yArr;
+    const xxyy = [];
+    const xx2 = [];
+    let xxyySum = 0;
+    let xx2Sum = 0;
+    if (xArr.length < 1 || xArr.length !== yArr.length) return null;
+    for (let i = 0; i < xArr.length; i++) {
+      xAvg = xAvg + xArr[i];
+      yAvg = yAvg + yArr[i];
+    }
+    xAvg = xAvg / xArr.length;
+    yAvg = yAvg / yArr.length;
+    for (let i = 0; i < xArr.length; i++) {
+      xx[i] = xArr[i] - xAvg;
+      yy[i] = yArr[i] - yAvg;
+      xxyy.push(xx[i] * yy[i]);
+      xx2.push(xx[i] * xx[i]);
+      xxyySum = xxyySum + xxyy[i];
+      xx2Sum = xx2Sum + xx2[i];
+    }
+    const b = xxyySum / xx2Sum;
+    const a = yAvg - b * xAvg;
+    return { a, b };
+  };
 
-/**
- * TODO
- * @param {Array} teams
- * @param {Array} matches
- * @return {Array}
- */
-const getSkillTable = (teams, matches) => {
-  const points = new Array(teams.length).fill(0);
-  matches.forEach(match => {
-    const homeIndex = teams.indexOf(match.teams[0]);
-    const homeSkillPoint = match.attempts[0] / match.attempts[1];
+  /**
+   * TODO
+   * @param {Object} match
+   * @return {Array}
+   */
+  const getMatchRating = match => {
+    return [
+      match.attempts[0] / match.attempts[1],
+      match.attempts[1] / match.attempts[0]
+    ];
+  };
 
-    const awayIndex = teams.indexOf(match.teams[1]);
-    const awaySkillPoint = 1 / homeSkillPoint;
+  /**
+   * TODO
+   * @param {Array} teams
+   * @param {Array} matches
+   * @return {Array}
+   */
+  const getSkillTable = (teams, matches) => {
+    const points = new Array(teams.length).fill(0);
+    matches.forEach(match => {
+      const matchRating = getMatchRating(match);
 
-    points[homeIndex] += homeSkillPoint;
-    points[awayIndex] += awaySkillPoint;
-  });
+      const homeIndex = teams.indexOf(match.teams[0]);
+      const homeSkillPoint = matchRating[0];
 
-  return points;
-};
+      const awayIndex = teams.indexOf(match.teams[1]);
+      const awaySkillPoint = matchRating[1];
 
-module.exports = {
-  getTeams,
-  getSkillTable
-};
+      points[homeIndex] += homeSkillPoint;
+      points[awayIndex] += awaySkillPoint;
+    });
+
+    return points;
+  };
+
+  /**
+   * Train
+   * @param {Object} match Match data
+   */
+  // Constructor.prototype.train = match => {};
+
+  /**
+   * Predict
+   * @param {Object} match Match data
+   */
+  // Constructor.prototype.predict = match => {};
+
+  return Constructor;
+})();
